8bce00ead54ffc10db3af046b772235ba3f56644
Akinobu Katou <e5843435@yahoo.co.jp>
2015-08-01 15:41:25 +0900

    Update birthday.rb

--- enpit-birthday/lib/enpit/birthday.rb
+++ enpit-birthday/lib/enpit/birthday.rb
@@ -2,17 +2,30 @@ require "enpit/birthday/version"
 
 module Enpit
   module Birthday
+    #　入力の初期値
+    year = '2000'
+    month = '12'
+    day = '31'
+    
     # 入力部
+    def input
+      
+    end
     
     # wikipedia呼び出し部
+    def wikipedia(year, month, day)
     
+    end
     
     # twitter呼び出し部
+    def twitter(year, month ,day)
     
+    end
     
     # テキスト出力部
-    
-    
+    def output
+      
+    end
     
   end
 end
